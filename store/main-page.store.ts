765ed76153f9487bdac4acc2cd348c097e378961
@zakharovda95 <zakharovda95@gmail.com>
2023-03-24 12:06:58 +0300

    request Call

--- store/main-page.store.ts
+++ store/main-page.store.ts
@@ -6,20 +6,31 @@ export const useMainPageStore = defineStore('main', {
   actions: {
     async call(body: CallFormData) {
       try {
-        const config = useRuntimeConfig();
-        const res = await useAsyncData('call', () =>
-          $fetch(`${config.public.BACK_API}/api/apartment`, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-            method: 'POST',
-            body: {
-              'apartment': '26',
-            },
-          }),
-        );
-
-        console.log(res);
+        // const config = useRuntimeConfig();
+        // const res = await useAsyncData('call', () =>
+        //   $fetch(`https://iat.ru/api/site-service-calculator/step1`, {
+        //     method: 'POST',
+        //     headers: {
+        //       ContentType: 'multipart/form-data',
+        //     },
+        //     body: {
+        //       id: 1,
+        //     },
+        //   }),
+        // );
+        // const res1 = await useAsyncData('call', () =>
+        //   $fetch(`${config.public.BACK_API}/api/call`, {
+        //     method: 'POST',
+        //     headers: {
+        //       ContentType: 'multipart/form-data',
+        //     },
+        //     body: {
+        //       name: body.name,
+        //       phone: body.phone,
+        //     },
+        //   }),
+        // );
+        // console.log(res.data.value!.data);
       } catch (err) {
         console.log(err);
       }
